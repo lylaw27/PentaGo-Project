ddf391b8935b19d3d07adeb3ed18fe5038f58961
lylaw27 <lylaw27@gmail.com>
2021-09-23 11:59:40 +0800

    all

--- api/PropertyListings.js
+++ api/PropertyListings.js
@@ -2,7 +2,7 @@ const express = require('express');
 const multer = require('multer');
 const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const router = express.Router();
-const Listings = require('../model/propertySchema.js');
+const Listings = require('../model/propertyschema.js');
 const cloudinary = require('cloudinary').v2;
 
 cloudinary.config({
