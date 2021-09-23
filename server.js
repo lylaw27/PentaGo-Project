be7ab21d7874d728053ad3e2f80e4e56439a8363
lylaw27 <lylaw27@gmail.com>
2021-09-23 16:29:55 +0800

    all

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use('/api',propertyListings);
 if (process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname, '/client/build')));
     app.get('*', function(req,res) {
-		res.sendFile(path.resolve(__dirname +'/app/client/build/index.html'));
+		res.sendFile(path.resolve(__dirname +'/client/build/index.html'));
     });
 }
 
