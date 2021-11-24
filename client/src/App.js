b091ed57d5e4d00a45f78d35f3dd9e728104eecb
lylaw27 <lylaw27@gmail.com>
2021-11-24 15:53:58 +0000

    hi

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import './css/style.css';
-import './css/about.css';
 import './css/fix.css';
-import './css/contact.css';
 import './css/blog-admin.css';
 import './css/details.css';
 import './css/upload.css';
-import './css/pagination.css'
+import './css/pagination.css';
 
 import {
     BrowserRouter as Router,
@@ -16,11 +14,10 @@ import {
 } from 'react-router-dom';
 
 import Home from "./pages-blog/index.jsx";
-import Aboutus from "./pages-blog/aboutus.jsx";
 import Blogcontent from "./pages-blog/blog-content.jsx";
 import CreateBlog from "./pages-admin/createblog.jsx"
 import EditBlog from "./pages-admin/editblog.jsx"
-import AdminBlog from "./pages-admin/home.jsx"
+import AdminBlog from "./pages-admin/adminhome.jsx"
 import PageNotFound from "./pagenotfound"
 
 class App extends React.Component{
@@ -29,12 +26,12 @@ class App extends React.Component{
             <Router>
                 <Switch>
                     <Route exact path='/' component={Home}/>
-                    <Route exact path='/aboutus' component={Aboutus}/>
                     <Route exact path='/admin/upload' component={CreateBlog}/>
                     <Route exact path='/admin/upload/:blogId' component={EditBlog}/>
                     <Route exact path='/admin' component={AdminBlog}/>
                     <Route path='/404' component={PageNotFound}/>
                     <Route exact path='/:blogId' component={Blogcontent}/>
+                    <Route exact path='/admin/:blogId' component={Blogcontent}/>
                     <Redirect from='/' to='/?page=1'/>
                     <Redirect from='*' to='/404' />
                 </Switch>
